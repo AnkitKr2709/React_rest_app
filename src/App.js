048c1e8666937f5a5fe22e8d3b3638c5e9fed799
AnkitKr2709 <kumarankitshah27@gmail.com>
2023-07-18 10:25:06 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import React from "react";
+
+// const App = () => {
+//   return (
+//     <>// react.format
+//       <MyName />
+//       <br />
+//       Welcome to Ankit 's Channel
+//     </>
+//   );
+// };
+
+// const MyName = () => {
+//   return <>Ankit Kumar Shah</>;
+// };
+// export default App;
+
+
+import React from "react";
+import Resturant from "./components/basics/Resturant";
+
+const App = () => {
+  return (
+    <>
+      <Resturant />
+    </>
+  );
+};
+
+export default App;
